8e59351e8f0c106c37397b964571439f7b446ba8
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-04 14:24:24 -0700

    started adding interactive. Start Quiz button works now

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
     
 /*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
 
-  	});
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+/*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
+
+/*-- Start quiz when button is pushed --*/
+    
+    $("#startQuiz").click(function() {
+        $("#startQuiz").hide();
+        $(".q1Header").show();
+        $(".questions p").show();
+        $(".answers").show();
+        $("input").show();
+        $("#submitAnswer").show();
+        $("#progress20").show();
+        $("#progress40").show();
+        $("#progress60").show();
+        $("#progress80").show();
+    });
 
 });
