ffebd2f9f106a08560757a04dead71ffa7888554
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-04 20:46:47 -0700

    implementing js after button is pressed and question 1 appears

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 /*-- Start quiz when button is pushed for non-interactive version 
     
     $("#startQuiz").click(function() {
-        $(".q1Header").show();
+        $(".qHeader").show();
         $(".questions p").show();
         $(".answers").show();
         $("input").show();
@@ -29,53 +29,51 @@ $(document).ready(function() {
     }); 
     --*/
 
-/*--quiz questions array--*/
+/*--quiz questions and answer array--*/
     var question1 = new Object();
     question1.title = "Question 1";
     question1.question = "Which company does Elon Musk own?";
     question1.answers = ["Dell", "Basecamp", "Tesla", "GE"];
     question1.correct = 2;
     
-    var questionList = [{
-        title: "Question 1",
-        question: "Which company does Elon Musk own?",
-        answers: ["Dell", "Basecamp", "Tesla", "GE"], 
-        qNum: 0, 
-        correct: 2
-        },
-        {
-        title: "Question 2",
-        question: "What group typically invests in startup companies?",
-        answers: ["Venture Capital", "Private Equity", "Hedge Funds", "Parents"], 
-        qNum: 1, 
-        correct: 0
-        }, 
-        {
-        title: "Question 3",    
-        question: "Which company provides breakfast, lunch, and dinner for employees?",
-        answers: ["Yahoo", "Goldman Sachs", "Patagonia", "Google"],
-        qNum: 2, 
-        correct: 3    
-        }, 
-        {
-        title: "Question 4",    
-        question: "CMO stands for...?",
-        answers: ["Chief Me Officer", "Chief Market Officer", "Chief Marketing Officer", "Chief Merge Officer"],
-        qNum: 3,
-        correct: 2
-        },
-        {
-        title: "Question 5",    
-        question: "What would Steve Jobs always wear for product announcements?",
-        answers: ["White collared shirt", "Black turtleneck", "Suit", "V-neck shirt"],
-        qNum: 4, 
-        correct: 1
-                  
-    }]
+    var question2 = new Object();
+    question2.title = "Question 2";
+    question2.question = "What group typically invests in startup companies?";
+    question2.answers = ["Venture Capital", "Private Equity", "Hedge Funds", "Parents"];
+    question2.correct = 0;
+    
+    var question3 = new Object();
+    question3.title = "Question 3";
+    question3.question = "Which company provides breakfast, lunch, and dinner for employees?";
+    question3.answers = ["Yahoo", "Goldman Sachs", "Patagonia", "Google"];
+    question3.correct = 3;
+    
+    var question4 = new Object();
+    question4.title = "Question 4";
+    question4.question = "CMO stands for...?";
+    question4.answers = ["Chief Me Officer", "Chief Market Officer", "Chief Marketing Officer", "Chief Merge Officer"];
+    question4.correct = 2;
+    
+    var question5 = new Object();
+    question5.title = "Question 5";
+    question5.question = "What would Steve Jobs always wear for product announcements?";
+    question5.answers = ["White collared shirt", "Black turtleneck", "Suit", "V-neck shirt"];
+    question5.correct = 1;
+    
+    var questions = [question1, question2, question3, question4, question5];
+       
+    /*-load first question when button is pressed-*/
     
     $("#startQuiz").click(function() {
         $("#startQuiz").fadeOut('slow');
-        
+        $(".qHeader").html(question1.title).fadeIn('slow');
+        $("#q").html(question1.question).fadeIn('slow');
+        $(".answers").html(question1.answers).fadeIn('slow');
+        $("#submitAnswer").show();
+        $("#progress20").show();
+        $("#progress40").show();
+        $("#progress60").show();
+        $("#progress80").show();
         
     });
     
