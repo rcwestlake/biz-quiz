be2c2fe508aef1b7b8e284e0f9bfc0526f254852
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-04 19:46:46 -0700

    started adding js - added questions and answer array

--- js/app.js
+++ js/app.js
@@ -26,4 +26,39 @@ $(document).ready(function() {
         $("#progress80").show();
     });
 
+/*--quiz questions array--*/
+    var questionList = [{
+        question: "Which company does Elon Musk own?",
+        answers: ["Dell", "Basecamp", "Tesla", "GE"], 
+        qNum: 0, 
+        correct: 2
+        },
+        {
+        question: "What group typically invests in startup companies?",
+        answers: ["Venture Capital", "Private Equity", "Hedge Funds", "Parents"], 
+        qNum: 1, 
+        correct: 0
+        }, 
+        {
+        question: "Which company provides breakfast, lunch, and dinner for employees?",
+        answers: ["Yahoo", "Goldman Sachs", "Patagonia", "Google"],
+        qNum: 2, 
+        correct: 3    
+        }, 
+        {
+        question: "CMO stands for...?",
+        answers: ["Chief Me Officer", "Chief Market Officer", "Chief Marketing Officer", "Chief Merge Officer"],
+        qNum: 3,
+        correct: 2
+        },
+        {
+         question: "What would Steve Jobs always wear for product announcements?",
+        answers: ["White collared shirt", "Black turtleneck", "Suit", "V-neck shirt"],
+        qNum: 4, 
+        correct: 1
+                  
+    }]
+        
+    
+
 });
